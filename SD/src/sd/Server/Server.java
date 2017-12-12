67a740fbf7b236f54cd149b7cd819b21e4cfa2e4
Yoda45 <ninjadinosaur.ilikecake@gmail.com>
2017-12-12 12:04:07 +0000

    Criada class Server.java e começada class Basejava

---
+++ SD/src/sd/Server/Server.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd.Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author yoda45
+ */
+public class Server {
+    private static final int port = 5000;
+    
+    public static void main(String[] args) throws IOException{
+        ServerSocket server = new ServerSocket(port);
+        
+        while(true){
+            Socket cliSocket = server.accept();
+            
+        }
+        
+        
+    }
+}
